fb030845fd6fd45668543e02d4ddde245e27d6d6
blengyel <blengyel@gmail.com>
2020-02-26 11:08:28 +0100

    Add files via upload

---
+++ seminars/03_network_intro/Network_R_intro.R
@@ -0,0 +1,7 @@
+# Intro Network Exercise with R
+
+# Install and call in package
+install.packages("igraph") # In case you have not installed the package yet. 
+                            #It is worth to repeat installation on a -say- yearly basis. Igraph R syntax is changing frequently.
+
+library(igraph) # Call this 
