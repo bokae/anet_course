00874bea569c8dbd217e0e4e6e2837cf1a32b4c0
Gergo Toth <60882378+gergtoth@users.noreply.github.com>
2020-03-02 16:31:26 +0100

    Delete Network_R_intro.R

--- seminars/03_network_intro/Network_R_intro.R
+++
@@ -1,7 +0,0 @@
-# Intro Network Exercise with R
-
-# Install and call in package
-install.packages("igraph") # In case you have not installed the package yet. 
-                            #It is worth to repeat installation on a -say- yearly basis. Igraph R syntax is changing frequently.
-
-library(igraph) # Call this 
